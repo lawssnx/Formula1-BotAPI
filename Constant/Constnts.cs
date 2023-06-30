d067af49d73b59196f91819145e8574b77c912af
anastasi <120751391+lawssnx@users.noreply.github.com>
2023-06-30 21:53:29 +0300

    Update Constnts.cs

--- Constant/Constnts.cs
+++ Constant/Constnts.cs
@@ -14,7 +14,7 @@ namespace RaceBT_API.Constant
 {
     public class constants
     {
-        public static string botId = "6271901154:AAGQxey2sFyiuprU0Vti44Vq8KRqBJkcZGk";
+        public static string botId = "";
         public static MongoClient mongoClient;
         public static IMongoDatabase database;
         public static IMongoCollection<BsonDocument> collection;
